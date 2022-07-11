85e15e3fe445f04aaf36cc0c276570989bf16304
Elielson Nascimento <nascimento.elielson@gmail.com>
2022-07-11 03:43:05 -0300

    the end

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const emptyBoard = Array(9).fill('');
-  const [ board, setBoard ] = useState(emptyBoard);
-  const [ currentPlayer, setCurrentPlayer ] = useState('O');
-  const [ winner, setWinner ] = useState();
+  const emptyBoard = Array(9).fill("");
+  const [board, setBoard] = useState(emptyBoard);
+  const [currentPlayer, setCurrentPlayer] = useState("O");
+  const [winner, setWinner] = useState();
 
   const handleClick = (index) => {
     if (winner) {
       alert(`${winner} venceu!!!`);
-    };
-    if (board[index] !== '') return null;
-    setBoard(board.map((item, itemIndex) => itemIndex === index ? currentPlayer : item));
-    setCurrentPlayer(currentPlayer === 'O' ? 'X' : 'O')
-  }
+    }
+    if (board[index] !== "") return null;
+    setBoard(
+      board.map((item, itemIndex) =>
+        itemIndex === index ? currentPlayer : item
+      )
+    );
+    setCurrentPlayer(currentPlayer === "O" ? "X" : "O");
+  };
 
   const checkWinner = () => {
     const possilbleWaysToWin = [
@@ -27,48 +31,71 @@ function App() {
       [board[2], board[5], board[9]],
 
       [board[0], board[4], board[8]],
-      [board[2], board[4], board[6]]
+      [board[2], board[4], board[6]],
     ];
 
-    possilbleWaysToWin.forEach(cells => {
-      if (cells.every(cell => cell === "O")) setWinner('O');
-      if (cells.every(cell => cell === "X")) setWinner('X');
+    possilbleWaysToWin.forEach((cells) => {
+      if (cells.every((cell) => cell === "O")) setWinner("O");
+      if (cells.every((cell) => cell === "X")) setWinner("X");
     });
-  }
+  };
 
   useEffect(() => {
-    if (winner) alert(`${winner} venceu!!!`);
+    if (winner) alert(`"${winner}" venceu!!!`);
     checkWinner();
-  })
+  });
 
   const resetGame = () => {
     // setCurrentPlayer(winner);
     // setBoard(emptyBoard);
     // setWinner(null);
     window.location.reload();
-  }
+  };
 
   return (
+    <div>
     <main>
-      <div className={`board ${winner ? 'game-over' : null}`}>
-        { board.map( (item, index) => (
-          <div 
-            key={ index }
-            className={ `cell ${item}` }
-            onClick={ () => handleClick(index) }
+      <div className={`board ${winner ? "game-over" : null}`}>
+        {board.map((item, index) => (
+          <div
+            key={index}
+            className={`cell ${item}`}
+            onClick={() => handleClick(index)}
           >
-            { item }
+            {item}
           </div>
-        )) }
+        ))}
       </div>
-      <button
-        className='resetGame'
-        onClick={resetGame}
-      >
+      <button className="resetGame" onClick={resetGame}>
         Reiniciar Jogo!
       </button>
     </main>
-  )
+    <footer id="Footer">
+      <a href="https://elielsondev.github.io/my-portfolio/" 
+      target="_blank" rel="noreferrer">
+        <h4 id="Created">Created by: Elielson Nascimento</h4>
+      </a>
+
+      <a href="https://www.linkedin.com/in/elielsondev/"
+      target="_blank" rel="noreferrer">
+        <img
+          width="60"
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-original-wordmark.svg"
+          alt="Linkedin"
+        />
+      </a>
+
+      <a href="https://github.com/elielsondev" 
+      target="_blank" rel="noreferrer">
+        <img
+          width="30"
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg"
+          alt="GitHub"
+        />
+      </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
