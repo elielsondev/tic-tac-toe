2ec3ba3a55be7e7065ee0c027bb8d2e1e35a8547
Elielson Nascimento <nascimento.elielson@gmail.com>
2022-07-11 01:58:39 -0300

    game complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const emptyBoard = Array(9).fill('');
+  const [ board, setBoard ] = useState(emptyBoard);
+  const [ currentPlayer, setCurrentPlayer ] = useState('O');
+  const [ winner, setWinner ] = useState();
+
+  const handleClick = (index) => {
+    if (winner) {
+      alert(`${winner} venceu!!!`);
+    };
+    if (board[index] !== '') return null;
+    setBoard(board.map((item, itemIndex) => itemIndex === index ? currentPlayer : item));
+    setCurrentPlayer(currentPlayer === 'O' ? 'X' : 'O')
+  }
+
+  const checkWinner = () => {
+    const possilbleWaysToWin = [
+      [board[0], board[1], board[2]],
+      [board[3], board[4], board[5]],
+      [board[6], board[7], board[9]],
+
+      [board[0], board[3], board[6]],
+      [board[1], board[4], board[7]],
+      [board[2], board[5], board[9]],
+
+      [board[0], board[4], board[8]],
+      [board[2], board[4], board[6]]
+    ];
+
+    possilbleWaysToWin.forEach(cells => {
+      if (cells.every(cell => cell === "O")) setWinner('O');
+      if (cells.every(cell => cell === "X")) setWinner('X');
+    });
+  }
+
+  useEffect(() => {
+    if (winner) alert(`${winner} venceu!!!`);
+    checkWinner();
+  })
+
+  const resetGame = () => {
+    setCurrentPlayer(winner);
+    setBoard(emptyBoard);
+    setWinner(null);
+  }
+
   return (
     <main>
-      <h1 className='title'>
-        JOGO DA VELHA
-      </h1> 
-      <div className='board'>
-        <div className='cell'>.</div>
-        <div className='cell'>.</div>
-        <div className='cell'>.</div>
-        <div className='cell'>.</div>
-        <div className='cell'>.</div>
-        <div className='cell'>.</div>
-        <div className='cell'>.</div>
-        <div className='cell'>.</div>
-        <div className='cell'>.</div>
-        <div className='cell'>.</div>
+      <div className={`board ${winner ? 'game-over' : null}`}>
+        { board.map( (item, index) => (
+          <div 
+            key={ index }
+            className={ `cell ${item}` }
+            onClick={ () => handleClick(index) }
+          >
+            { item }
+          </div>
+        )) }
       </div>
+      <button
+        className='resetGame'
+        onClick={resetGame}
+      >
+        Reiniciar Jogo!
+      </button>
     </main>
   )
 }
